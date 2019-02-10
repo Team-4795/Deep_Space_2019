a88489f33b08883e27fabfe57124f2b0de656633
Charles Tsai <>
2019-02-10 15:00:26 -0500

    Fixed auto intake command

--- src/main/java/frc/robot/commands/TakeIn.java
+++ src/main/java/frc/robot/commands/TakeIn.java
@@ -11,7 +11,10 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 public class TakeIn extends Command {
-  public TakeIn() {
+  
+  double Speed;
+  public TakeIn(double speed) {
+    Speed = speed;
     requires(Robot.intake);
   }
 
@@ -23,7 +26,7 @@ public class TakeIn extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.intake.TakeIn();
+    Robot.intake.set(Speed);
   }
 
   // Make this return true when this Command no longer needs to run execute()
